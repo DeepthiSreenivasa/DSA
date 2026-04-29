0710ffce0d877b2ea0dc4d5da1f0eaa37ade1b01
Deepthi S <121350337+DeepthiSreenivasa@users.noreply.github.com>
2026-04-29 11:35:29 +0530

    Add countDigit function to count number of digits
    Implement a function to count the number of digits in a number, handling negative values.

---
+++ countNumberOfDigits.js
@@ -0,0 +1,30 @@
+/*
+ * 
+ * Author: Deepthi S
+ * Notes :
+ * 
+ * Math.abs => convert every number to positive number
+ * Math.floor=>Round the number to lowest
+ * Math.ciel=> Round the number to highest
+ * Math.round=>Round the number to lower(when <=0.4) or higher(when >=0.5) 
+ * 
+ */
+
+
+function countDigit(num) {
+
+    let count = 0;
+
+    if (num === 0) return 1;
+
+    num =  Math.abs(num);
+
+    while (num > 0) {
+        num = Math.floor(num / 10);
+        count++
+    }
+    return count;
+}
+
+const result = countDigit(-989);
+console.log(result);
